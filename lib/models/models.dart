c798857038fd9c96ab6417bdaebaa65256389c1e
babe-saleh-mahfoud <babesalehmahfoud@gmail.com>
2024-03-19 15:07:52 +0000

    changes

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,13 +1,31 @@
+import 'package:equatable/equatable.dart';
+
 enum SquareContent {
   whitePiece,
   blackPiece,
   empty,
 }
 
-class BoardPosition {
+class BoardPosition extends Equatable {
   final int rIndex;
   final int cIndex;
-  BoardPosition({required this.cIndex, required this.rIndex});
+
+  const BoardPosition({required this.cIndex, required this.rIndex});
 
   int sum() => cIndex + rIndex;
+
+  @override
+  List<Object?> get props => [rIndex, cIndex];
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is BoardPosition &&
+        other.rIndex == rIndex &&
+        other.cIndex == cIndex;
+  }
+
+  @override
+  int get hashCode => rIndex.hashCode ^ cIndex.hashCode;
 }
