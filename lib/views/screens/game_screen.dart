860f7ae13fe98a38fc90ba677dc12c75fb56f72c
babe-saleh-mahfoud <babesalehmahfoud@gmail.com>
2024-03-28 17:42:30 +0000

    changes

--- lib/views/screens/game_screen.dart
+++ lib/views/screens/game_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:rim_chess/views/components/common_components.dart';
-import 'package:rim_chess/constants.dart';
 import 'package:rim_chess/controllers/game_state_provider.dart';
 import 'package:rim_chess/models/models.dart';
 
