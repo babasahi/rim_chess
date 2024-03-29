4330d2a7e4d8b57e0dc8698aa60d854a74554e11
babe-saleh-mahfoud <babesalehmahfoud@gmail.com>
2024-03-29 08:07:46 +0000

    added screens

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:rim_chess/models/models.dart';
 
+TextStyle kSelectedIconLabelStyle = const TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 14,
+  color: kPrimaryColor,
+);
+TextStyle kUnSelectedIconLabelStyle = const TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 14,
+  color: kIconsColor,
+);
+
+TextStyle kLogoTextStyle = const TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 48,
+  color: kPrimaryColor,
+);
+
+TextStyle kNormalTextStyle = const TextStyle(
+  fontSize: 16,
+  color: Colors.black,
+);
+
+TextStyle kHeadingStyle = TextStyle(
+  fontSize: 20,
+  color: Colors.black.withOpacity(0.9),
+  fontWeight: FontWeight.w600,
+);
+// Colors
+
+const Color kPrimaryColor = Color(0xFF4A4A4A);
+const Color kBackgroundColor = Color(0xFFFFFFFF);
+const Color kSecondaryColor = Color(0xFFF5F5F5);
+const Color kTertiaryColor = Color(0xFFDD1D45);
+const Color kDarkBackgroundColor = Color(0xFFF6F6F6);
+const Color kIconsColor = Color(0XFFD9D9D9);
+const Color kBorderColor = Color(0XFFC9C9C9);
+const Color kYellowColor = Color(0xFFFEF8E2);
+
+Color kAlertColor = const Color(0xFFF51010);
+Color kSuccessColor = const Color(0xFF22C55E);
+
+Color kLoadingTransparentColor = Colors.white.withOpacity(0.5);
+
+// layout constants
+
+double screenWidth(BuildContext context) => MediaQuery.sizeOf(context).width;
+double screenHeight(BuildContext context) => MediaQuery.sizeOf(context).height;
+EdgeInsetsGeometry screenPadding(BuildContext context) {
+  return EdgeInsets.symmetric(
+    horizontal: screenWidth(context) * 0.066,
+    vertical: screenHeight(context) * 0.015,
+  );
+}
+
 double boardWidth(BuildContext context) =>
     MediaQuery.of(context).size.height * 0.8;
 
---
+++ lib/scaffold_screen.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:rim_chess/constants.dart';
+import 'package:rim_chess/views/screens/home_screen.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final List<Widget> _screens = [
+    const HomeScreen(),
+    const LiveGamesScreen(),
+    const MyGamesScreen(),
+    const ProfileScreen(),
+  ];
+  final PageController _pageController = PageController(initialPage: 0);
+  int _index = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kBackgroundColor,
+      body: PageView(
+        physics: const NeverScrollableScrollPhysics(),
+        controller: _pageController,
+        children: _screens,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: kBackgroundColor,
+        currentIndex: _index,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        type: BottomNavigationBarType.fixed,
+        onTap: (value) {
+          setState(() {
+            _index = value;
+          });
+          _pageController.jumpToPage(
+            value,
+          );
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: CircleAvatar(
+              backgroundColor: _index == 0 ? kPrimaryColor : kBackgroundColor,
+              radius: 20,
+              child: SvgPicture.asset(
+                'assets/images/icons/home.svg',
+                color: _index == 0 ? kBackgroundColor : kIconsColor,
+                height: 20,
+                width: 20,
+              ),
+            ),
+            label: 'home'.tr(),
+          ),
+          BottomNavigationBarItem(
+            icon: CircleAvatar(
+              backgroundColor: _index == 1 ? kPrimaryColor : kBackgroundColor,
+              radius: 20,
+              child: SvgPicture.asset(
+                'assets/images/icons/shop.svg',
+                color: _index == 1 ? kBackgroundColor : kIconsColor,
+                width: 21,
+                height: 19,
+              ),
+            ),
+            label: 'orders',
+          ),
+          BottomNavigationBarItem(
+            icon: CircleAvatar(
+              backgroundColor: _index == 2 ? kPrimaryColor : kBackgroundColor,
+              radius: 20,
+              child: SvgPicture.asset(
+                'assets/images/icons/calendar.svg',
+                color: _index == 2 ? kBackgroundColor : kIconsColor,
+                height: 22.25,
+                width: 21,
+              ),
+            ),
+            label: 'support',
+          ),
+          BottomNavigationBarItem(
+            icon: CircleAvatar(
+              backgroundColor: _index == 3 ? kPrimaryColor : kBackgroundColor,
+              radius: 20,
+              child: SvgPicture.asset(
+                'assets/images/icons/membership.svg',
+                color: _index == 3 ? kBackgroundColor : kIconsColor,
+                height: 25,
+                width: 24,
+              ),
+            ),
+            label: 'support',
+          ),
+          BottomNavigationBarItem(
+            icon: CircleAvatar(
+              backgroundColor: _index == 4 ? kPrimaryColor : kBackgroundColor,
+              radius: 20,
+              child: SvgPicture.asset(
+                'assets/images/icons/options.svg',
+                color: _index == 4 ? kBackgroundColor : kIconsColor,
+                width: 19.5,
+                height: 15.5,
+              ),
+            ),
+            label: 'support',
+          ),
+        ],
+      ),
+    );
+  }
+}
