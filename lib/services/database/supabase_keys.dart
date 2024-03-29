fdeca1fdd437fe924ec785e4acd6e81385b2d39f
babe-saleh-mahfoud <babesalehmahfoud@gmail.com>
2024-03-29 08:58:58 +0000

    changes

--- lib/services/database/supabase_keys.dart
+++
@@ -1,3 +0,0 @@
-const String supabaseURL = 'https://brnvqnptprafkhiwxtib.supabase.co';
-const String supabaseKey =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJybnZxbnB0cHJhZmtoaXd4dGliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE2NjMxODQsImV4cCI6MjAyNzIzOTE4NH0._bhXoMjQyv7SSxIDKpl4V1CwPulcWxlkqVv14IBoErg';
