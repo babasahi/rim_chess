ff8cba89dd86025e3618ae73ace4b24a9c3e9bfd
babe-saleh-mahfoud <babesalehmahfoud@gmail.com>
2024-03-29 08:50:25 +0000

    changes

--- lib/services/database/database.dart
+++ lib/services/database/database.dart
@@ -1,8 +1,5 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 const String avatarUrl = 'https://source.boringavatars.com/beam/100';
-const String supabaseURL = 'https://brnvqnptprafkhiwxtib.supabase.co';
-const String supabaseKey =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJybnZxbnB0cHJhZmtoaXd4dGliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE2NjMxODQsImV4cCI6MjAyNzIzOTE4NH0._bhXoMjQyv7SSxIDKpl4V1CwPulcWxlkqVv14IBoErg';
 
 final supabase = Supabase.instance.client;
