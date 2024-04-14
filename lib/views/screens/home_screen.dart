10e6cfeafa4205bec55e016b8e1f04bdc6c67d53
babe-saleh-mahfoud <babesalehmahfoud@gmail.com>
2024-04-14 19:53:59 +0000

    changes

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -9,54 +9,64 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kSecondaryColor,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Row(
-              children: [],
-            ),
-            GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const GameScreen(),
-                  ),
-                );
-              },
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.deepPurpleAccent,
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.deepPurpleAccent.shade400,
-                        offset: const Offset(0, 6),
-                        spreadRadius: BorderSide.strokeAlignInside,
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 14,
+                      backgroundColor: kPrimaryColor,
+                    )
+                  ],
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const GameScreen(),
                       ),
-                      BoxShadow(
-                        color: Colors.deepPurpleAccent.shade400,
-                        offset: const Offset(0, 6),
-                        spreadRadius: BorderSide.strokeAlignInside,
-                      )
-                    ]),
-                height: 38,
-                width: MediaQuery.of(context).size.width * 0.6,
-                child: const Center(
-                  child: Text(
-                    'ابدا اللعب',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
-                      fontFamily: 'DINNext',
-                      fontWeight: FontWeight.bold,
+                    );
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.deepPurpleAccent,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.deepPurpleAccent.shade400,
+                            offset: const Offset(0, 6),
+                            spreadRadius: BorderSide.strokeAlignInside,
+                          ),
+                          BoxShadow(
+                            color: Colors.deepPurpleAccent.shade400,
+                            offset: const Offset(0, 6),
+                            spreadRadius: BorderSide.strokeAlignInside,
+                          )
+                        ]),
+                    height: 38,
+                    width: MediaQuery.of(context).size.width * 0.6,
+                    child: const Center(
+                      child: Text(
+                        'ابدا اللعب',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontFamily: 'DINNext',
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
