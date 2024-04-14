06a7f2897b57ff8c039179f79f511c26fdeecba6
babe-saleh-mahfoud <babesalehmahfoud@gmail.com>
2024-04-14 19:30:34 +0000

    changes to profilepictur e

--- lib/views/screens/profile_screen.dart
+++ lib/views/screens/profile_screen.dart
@@ -10,6 +10,10 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: SafeArea(
+        child: Column(),
+      ),
+    );
   }
 }
